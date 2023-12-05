e25ac7283dc901089d18cd0823654a5c4f61b457
Bac Nguyen <nxbac.dev@gmail.com>
2023-12-05 21:35:24 +0700

    #12 Load config from env with viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,12 +6,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/bacnx/simplebank/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var (
@@ -20,8 +16,11 @@ var (
 )
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.GetConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
--- main.go
+++ main.go
@@ -6,22 +6,21 @@ import (
 
 	"github.com/bacnx/simplebank/api"
 	db "github.com/bacnx/simplebank/db/sqlc"
+	"github.com/bacnx/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.GetConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	server.Start(serverAddress)
+	server.Start(config.ServerAddress)
 }
