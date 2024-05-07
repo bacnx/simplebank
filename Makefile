7ae48eea994b1cd3181f1d67d0bb1f21e30f4395
Bac Nguyen <nxbac.dev@gmail.com>
2024-05-07 21:35:24 +0700

    implement gRPC-Gateway

--- Makefile
+++ Makefile
@@ -47,8 +47,10 @@ mock:
 	mockgen -destination ./db/mock/store.go -package mockdb github.com/bacnx/simplebank/db/sqlc Store
 
 proto:
-	protoc --proto_path=proto --go_out=pb --go_opt=paths=source_relative \
-    --go-grpc_out=pb --go-grpc_opt=paths=source_relative \
+	protoc --proto_path ./proto \
+		--go_out ./pb --go_opt paths=source_relative \
+    --go-grpc_out ./pb --go-grpc_opt paths=source_relative \
+		--grpc-gateway_out ./pb --grpc-gateway_opt paths=source_relative \
     proto/*.proto
 
 evans:
--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net"
+	"net/http"
 
 	"github.com/bacnx/simplebank/api"
 	db "github.com/bacnx/simplebank/db/sqlc"
 	"github.com/bacnx/simplebank/gapi"
 	"github.com/bacnx/simplebank/pb"
 	"github.com/bacnx/simplebank/util"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
@@ -26,6 +30,7 @@ func main() {
 	}
 	store := db.NewStore(conn)
 
+	go runRrpcGatewayServer(config, store)
 	runGrpcServer(config, store)
 }
 
@@ -50,12 +55,43 @@ func runGrpcServer(config util.Config, store db.Store) {
 
 	listener, err := net.Listen("tcp", config.GRPCServerAddress)
 	if err != nil {
-		log.Fatal("cannot create listter")
+		log.Fatal("cannot create listener:", err)
 	}
 
 	log.Printf("start gRPC server at %s", listener.Addr().String())
 	err = grpcServer.Serve(listener)
 	if err != nil {
-		log.Fatal("cannot start gRPC server")
+		log.Fatal("cannot start gRPC server:", err)
+	}
+}
+
+func runRrpcGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create gapi server:", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	mux := runtime.NewServeMux(
+		runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+			MarshalOptions: protojson.MarshalOptions{
+				UseProtoNames: true,
+			},
+			UnmarshalOptions: protojson.UnmarshalOptions{
+				DiscardUnknown: true,
+			},
+		}),
+	)
+	err = pb.RegisterSimplebankHandlerServer(ctx, mux, server)
+	if err != nil {
+		log.Fatal("cannot register handler:", err)
+	}
+
+	log.Printf("start gRPC-Gateway server at %s", config.HTTPServerAddress)
+	err = http.ListenAndServe(config.HTTPServerAddress, mux)
+	if err != nil {
+		log.Fatal("cannot start gRPC-Gateway server:", err)
 	}
 }
