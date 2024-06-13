a8273bbb861268b026fe39ccf87019905b0eb2f9
Bac Nguyen <nxbac.dev@gmail.com>
2024-06-13 22:22:05 +0700

    use db transaction for create user

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -3,13 +3,13 @@ package db
 import (
 	"context"
 	"database/sql"
-	"errors"
 	"fmt"
 )
 
 type Store interface {
 	Querier
 	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+	CreateUserTx(ctx context.Context, arg CreateUserTxParams) (CreateUserTxResult, error)
 }
 
 type SQLStore struct {
@@ -34,93 +34,12 @@ func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) erro
 	err = fn(q)
 	if err != nil {
 		if rbErr := tx.Rollback(); rbErr != nil {
-			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+			return fmt.Errorf("tx err: %w, rb err: %w", err, rbErr)
 		}
 		return err
 	}
 
 	return tx.Commit()
-}
-
-type TransferTxParams struct {
-	FromAccountID int64 `json:"from_account_id"`
-	ToAccountID   int64 `json:"to_account_id"`
-	Amount        int64 `json:"amount"`
-}
-
-type TransferTxResult struct {
-	Transfer    Transfer `json:"transfer"`
-	FromAccount Account  `json:"from_account"`
-	ToAccount   Account  `json:"to_account"`
-	FromEntry   Entry    `json:"from_entry"`
-	ToEntry     Entry    `json:"to_entry"`
-}
-
-// TransferTx performs a money transfer from one account to the other.
-// It creates a transfer record, add account entries, and update account's balance within a single database transaction
-func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
-	var result TransferTxResult
-
-	err := store.execTx(ctx, func(q *Queries) error {
-		var err error
-
-		var account1, account2 Account
-		if arg.FromAccountID < arg.ToAccountID {
-			account1, account2, err = getTwoAccounts(ctx, q, arg.FromAccountID, arg.ToAccountID)
-		} else {
-			account2, account1, err = getTwoAccounts(ctx, q, arg.ToAccountID, arg.FromAccountID)
-		}
-
-		if err != nil {
-			return err
-		}
-
-		if account1.Currency != account2.Currency {
-			return errors.New("the currencies of the two accounts are not same")
-		}
-
-		// if account1.Balance < arg.Amount {
-		// 	return errors.New("from account's balance is not enough")
-		// }
-
-		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
-			FromAccountID: arg.FromAccountID,
-			ToAccountID:   arg.ToAccountID,
-			Amount:        arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-
-		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
-			AccountID: arg.FromAccountID,
-			Amount:    -arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-
-		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
-			AccountID: arg.ToAccountID,
-			Amount:    arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-
-		if arg.FromAccountID < arg.ToAccountID {
-			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, account1.Balance-arg.Amount, arg.ToAccountID, account2.Balance+arg.Amount)
-		} else {
-			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, account2.Balance+arg.Amount, arg.FromAccountID, account1.Balance-arg.Amount)
-		}
-		if err != nil {
-			return err
-		}
-
-		return nil
-	})
-
-	return result, err
 }
 
 func getTwoAccounts(
--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -2,6 +2,7 @@ package gapi
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	db "github.com/bacnx/simplebank/db/sqlc"
@@ -27,14 +28,32 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 		return nil, status.Errorf(codes.Internal, "failed to hash password: %s", err.Error())
 	}
 
-	arg := db.CreateUserParams{
-		Username:       req.GetUsername(),
-		HashedPassword: hashedPassword,
-		FullName:       req.GetFullName(),
-		Email:          req.GetEmail(),
+	arg := db.CreateUserTxParams{
+		CreateUserParams: db.CreateUserParams{
+			Username:       req.GetUsername(),
+			HashedPassword: hashedPassword,
+			FullName:       req.GetFullName(),
+			Email:          req.GetEmail(),
+		},
+		AfterCreate: func(user db.User) error {
+			taskPayload := &worker.PayloadSendVerifyEmail{
+				Username: user.Username,
+			}
+			opts := []asynq.Option{
+				asynq.MaxRetry(10),
+				asynq.ProcessIn(10 * time.Second),
+				asynq.Queue(worker.QueueCretical),
+			}
+			err = server.taskDistributor.DistributeTaskSendVerifyEmail(ctx, taskPayload, opts...)
+			if err != nil {
+				return fmt.Errorf("failed to distribute task to send verify email: %w", err)
+			}
+
+			return nil
+		},
 	}
 
-	user, err := server.store.CreateUser(ctx, arg)
+	resultTx, err := server.store.CreateUserTx(ctx, arg)
 	if err != nil {
 		if pqErr, ok := err.(*pq.Error); ok {
 			switch pqErr.Code.Name() {
@@ -45,22 +64,8 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err.Error())
 	}
 
-	// TODO: use db transaction
-	taskPayload := &worker.PayloadSendVerifyEmail{
-		Username: user.Username,
-	}
-	opts := []asynq.Option{
-		asynq.MaxRetry(10),
-		asynq.ProcessIn(10 * time.Second),
-		asynq.Queue(worker.QueueCretical),
-	}
-	err = server.taskDistributor.DistributeTaskSendVerifyEmail(ctx, taskPayload, opts...)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to distribute task to send verify email: %s", err)
-	}
-
 	return &pb.CreateUserResponse{
-		User: convertUser(user),
+		User: convertUser(resultTx.User),
 	}, nil
 }
 
